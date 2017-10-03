e578187fa92a5b98b5a8b9c66db142bc0d872f4c
bigbaldy1128 <bigbaldy@163.com>
2017-10-03 11:38:08 +0800

    modify png

---
+++ hdfs/src/main/java/com/wjz/hdfs/HdfsApplication.java
@@ -0,0 +1,24 @@
+package com.wjz.hdfs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsApplication implements CommandLineRunner {
+
+	@Autowired
+	private FsShell shell;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		for(String text :shell.text("/usr/test")){
+			System.out.println(text);
+		}
+	}
+}
