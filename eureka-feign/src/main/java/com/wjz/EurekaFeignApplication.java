2feea37cbdf0cbea6403624112a58e3d878c9c4a
wjz <wjz@163.com>
2017-10-16 13:41:32 +0800

    add turbine

--- eureka-feign/src/main/java/com/wjz/EurekaFeignApplication.java
+++ eureka-feign/src/main/java/com/wjz/EurekaFeignApplication.java
@@ -2,6 +2,7 @@ package com.wjz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class EurekaFeignApplication {
 
 	public static void main(String[] args) {
